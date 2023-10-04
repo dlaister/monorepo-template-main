d1492f48e72071c531e50b538c1adbcfce5ac5e3
dlaister <laister.d@northeastern.edu>
2023-10-03 19:59:21 -0700

    bad attempt at refactoring/understanding/testing

--- Assignment3/part1/gilded_rose.py
+++ Assignment3/part1/gilded_rose.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-#--------------------GLOBAL CONSTANTS--------------------#
-#TODO Global Constants, min, max values, passed sell-by-date (possible others)
-
-
 #--------------------CLASS (CANNOT BE MODIFIED, BUT CAN BE EXTENDED)--------------------#
 class Item:
     """ DO NOT CHANGE THIS CLASS!!!"""
@@ -23,108 +19,92 @@ class GildedRose(object):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
 
-    # TODO - helper function to select appropriate item to update???
+    #--------------------FUNCTIONS--------------------#
     """
-    Pseudo for new update_quality function to select an item to update:
-    
+    New update_quality function to select an item to appropriately asses the sell_in and quality.
+    """
     def update_quality(self):
-        for an item in items:
-            if item == Aged Brie
-                update item aged brie
-            else if item == Backstage passes to a TAFKAL80ETC concert
-                update Backstage passes to a TAFKAL80ETC concert
-            else if item == Conjured Mana Cake
-                update item Conjured Mana Cake
-            else item == Sulfas, Hand of Ragnaros
-                update item Sulfas, Hand of Ragnaros        
-    """
+        update_item = Update_item()
 
-    # --------------------ORIGINAL CODE--------------------#
-    # def update_quality(self):
-    #     for item in self.items:
+        for item in self.items:
+            if item.name == "Aged Brie":
+                update_item.aged_brie(item)
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                update_item.backstage_passes(item)
+            elif item.name == "Conjured Mana Cake":
+                update_item.conjured_mana_cake(item)
+            elif item.name == "Sulfuras, Hand of Ragnaros":
+                update_item.sulfuras(item)
+            else:
+                print(">>> Incorrect item <<<")
 
-              #item quality
-    #         if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-    #             if item.quality > 0: #min number (item quality more than 0, do the below)
-    #                 if item.name != "Sulfuras, Hand of Ragnaros":
-    #                     item.quality = item.quality - 1
-
-    #         else:
-    #             if item.quality < 50: #max number (item quality less than 50, do the below)
-    #                 item.quality = item.quality + 1
-    #                 if item.name == "Backstage passes to a TAFKAL80ETC concert":
-    #                     if item.sell_in < 11:
-    #                         if item.quality < 50:
-    #                             item.quality = item.quality + 1
-    #                     if item.sell_in < 6:
-    #                         if item.quality < 50:
-    #                             item.quality = item.quality + 1
-
-              #item sell-by-date
-    #         if item.name != "Sulfuras, Hand of Ragnaros":
-    #             item.sell_in = item.sell_in - 1 #passed_sell (if the item date is -1, do the below)
-    #
-    #         if item.sell_in < 0:
-    #             if item.name != "Aged Brie":
-    #                 if item.name != "Backstage passes to a TAFKAL80ETC concert":
-    #                     if item.quality > 0:
-    #                         if item.name != "Sulfuras, Hand of Ragnaros":
-    #                             item.quality = item.quality - 1
-    #                 #decrement item quality if Mana Cake
-    #                 else:
-    #                     item.quality = item.quality - item.quality
-                  #else increment other items by 1 until max (50) reached
-    #             else:
-    #                 if item.quality < 50:
-    #                     item.quality = item.quality + 1
 
 # --------------------CLASS (REFACTORED)--------------------#
-#TODO - class update
-#Needs helper functions (in the GildedRose class???) to select appropriate item to change the appropriate values.
-
 """
-Create class to update/modified functionality (also improves scalability/longevity)
-
-Class needs to have functions for each of the four items (Aged Brie, Backstage passes to a TAFKAL80ETC concert, Conjured Mana Cake, and Sulfuras, Hand of Ragnaros).
-
-Global constants will be needed (See #TODO Global Constants)
-
-Things to note:
-- All items have a SellIn value which denotes the number of days we have to sell the item
-- All items have a Quality value which denotes how valuable the item is
-* At the end of each day our system lowers both values for every item
-
-Additional things to note:
-* Once the sell by date has passed, Quality degrades TWICE as fast (double degradation)
-- The Quality of an item is never negative (min)
-* "Aged Brie" actually increases in Quality the older it gets (Brie is the only item to increase in quality)
-- The Quality of an item is NEVER MORE THAN 50 (max)
-* "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
-- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
---Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
---Quality drops to 0 after the concert
-
-Lastly:
-- "Conjured" items degrade in Quality twice as fast as normal items
-
-Function Aged Brie:
-increase in quality the older it gets until
-
-
-Function Backstage passes to a TAFKAL80ETC concert:
-increases in quality **increase DOUBLES when the are 10 to 6 days AND TRIPLES when there are 5 or less days and is 0 AFTER date** the older it gets
-
- 
-Function Conjured Mana Cake
-degrade in quality TWICE as fast as NORMAL items
-
-
-Function Sulfuras, Hand of Ragnaros:
-does not have a sell-by-date AND does not degrade!!! 
-no need for any changes here, just pass it
-
-
+Class to update the sell_in and quality of a specific item.
 """
+class Update_item(object):
 
+    #--------------------GLOBAL CONSTANTS--------------------#
+    MIN_QUALITY = 0
+    MAX_QUALITY = 50
+    PAST_DATE = -1
+    SINGLE_RATE = 1
+    DOUBLE_RATE = 2
+    TRIPLE_RATE = 3
 
+    #--------------------FUNCTIONS--------------------#
+    """
+    Function Aged Brie increases in quality the older it gets until itme quality is 50.
+    """
+    def aged_brie(self, item):
+        if item.sell_in > 0:
+            appreciation = self.SINGLE_RATE
+        else:
+            appreciation = self.DOUBLE_RATE
 
+        item.quality = min((item.quality + appreciation), self.MAX_QUALITY)
+        item.sell_in += self.PAST_DATE
+
+    """
+    Function Backstage passes to a TAFKAL80ETC concert increases in quality (increase DOUBLES when there are 10 to 6 days AND TRIPLES when there are 5 to 1 days and is 0 AFTER the date) the older it gets until AFTER the date.
+    """
+    def backstage_passes(self, item):
+        """
+        Helper function to get the quality based on the day of appreciation.
+        :param item: the item (ticket)
+        :return: the quality given the day
+        """
+        def get_quality(item, appreciation):
+            item.quality = min(item.quality + appreciation, self.MAX_QUALITY)
+
+            if item.sell_in > 10:
+                appreciation = self.SINGLE_RATE
+                get_quality(item, appreciation)
+            elif item.sell_in > 5:
+                appreciation = self.DOUBLE_RATE
+                get_quality(item, appreciation)
+            elif item.sell_in > 0:
+                appreciation = self.TRIPLE_RATE
+                get_quality(item, appreciation)
+            else:
+                item.quality = 0
+                item.sell_in += self.PAST_DATE
+
+    """
+    Function Conjured Mana Cake decreases in quality TWICE as fast as a NORMAL item.
+    """
+    def conjured_mana_cake(self, item):
+        if item.sell_in > 0:
+            depreciation = -2
+        else:
+            depreciation = -4
+
+        item.quality = max((item.quality + depreciation), self.MIN_QUALITY)
+        item.sell_in += self.PAST_DATE
+
+    """
+    Function Sulfuras, Hand of Ragnaros DOES NOT have a sell-by-date AND DOES NOT degrade (...no need for any changes, pass it).
+    """
+    def sulfuras(self, item):
+        pass
--- Assignment3/part1/test_gilded_rose.py
+++ Assignment3/part1/test_gilded_rose.py
@@ -12,13 +12,57 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEquals("fixme", items[0].name)
 
-    # --------------------DEFAULT TEST--------------------#
-    #TODO - Additional test per item - items need setups like junit testing, edge cases should be tested like day 0 and 50, and when value changes for the concert (10, 5, 0)
-    # Aged Brie
-    # Backstage passes to a TAFKAL80ETC concert
-    # Conjured Mana Cake
-    # Sulfuras, Hand of Ragnaros
+    # --------------------EDGE-CASE TEST--------------------#
+    """
+    Function to update quality over time.
+    """
+    def update_quality(obj = GildedRose, times = 1):
+        for i in range(times):
+            obj.update_quality()
 
+    """
+    Aged Brie
+    """
+    def test_aged_brie(self):
+        # Item instance
+        item = Item("Aged Brie", 1, 30)
+        gilded_rose = GildedRose([item])
+
+        # First day
+        gilded_rose.update_quality()
+        self.assertEqual(item.sell_in,0)
+        self.assertEqual(item.quality,31)
+
+        # Sell-by-date expired (day -1)
+        GildedRose.update_quality(gilded_rose, item.sell_in + 1)
+        self.assertEqual(item.sell_in, -1)
+
+
+        # 2x quality INCREASE
+
+    """
+    Backstage passes to a TAFKAL80ETC concert
+    """
+    # Item instance
+    # First day
+    # 2x for 10 or fewer days
+    # 3x for 5 or fewer days
+    # Sell-by-date expired (day -1)
+
+    """
+    Conjured Mana Cake
+    """
+    # Item instance
+    # First day
+    # Sell-by-date expired (day -1)
+    # 4x quality DECREASE
+
+    """
+    Sulfuras, Hand of Ragnaros
+    """
+    # Item instance
+    # First day
+    # Sell-by-date expired (day -1)
 
 if __name__ == '__main__':
     unittest.main()
